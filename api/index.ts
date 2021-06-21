4dfee7aa632638ff47a721438136a91800cf8ef1
Alan <boyce.alan15@gmail.com>
2021-06-21 19:51:26 -0400

    remove http server

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,11 @@
 import App from './App';
 import dotenv from 'dotenv';
-import http from 'http';
 import https from 'https';
 import fs from 'fs';
 
 //access to environment variables
 dotenv.config({ path: './config/.env' });
 
-const HTTP_PORT = process.env.HTTP_PORT;
 const HTTPS_PORT = process.env.HTTPS_PORT;
 
 //read private key - should be an env variable or dynamically set (ec2-user as ex.)
@@ -22,12 +20,7 @@ const credentials = { key: privateKey, cert: certificate };
 //build App with given port
 const app = App.buildApp();
 
-const httpServer = http.createServer(app);
-
 const httpsServer = https.createServer(credentials, app);
-
-//now listening for client requests 
-httpServer.listen(HTTP_PORT);
 
 //now listening for client requests 
 httpsServer.listen(HTTPS_PORT);
