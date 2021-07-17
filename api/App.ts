55447559e39d98553c9c327a3e594921484ce85a
Alan <boyce.alan15@gmail.com>
2021-07-17 04:18:19 -0400

    more auth

--- api/App.ts
+++ api/App.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import DeviceManagerController from './device-manager/DeviceManagerController';
-import { auth } from 'express-openid-connect';
+import { auth, requiresAuth } from 'express-openid-connect';
+import cors from 'cors';
 
 // Creates and configures an ExpressJS web server. Prevents sub-typing of this class.
 class App {
@@ -29,26 +30,34 @@ class App {
 
   // Configure Express middleware.
   private static middleware(): void {
+    const ISSUER_BASE_URL = process.env.ISSUER_BASE_URL;
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const PROD_BASE_URL = process.env.PROD_BASE_URL;
+    const DEV_BASE_URL = process.env.DEV_BASE_URL;
+    const SECRET = process.env.SECRET;
+
     if (this.app) {
       this.app.use(
         auth({
-          issuerBaseURL: 'https://YOUR_DOMAIN',
-          baseURL: 'https://YOUR_APPLICATION_ROOT_URL',
-          clientID: 'YOUR_CLIENT_ID',
-          secret: 'LONG_RANDOM_STRING',
+          authRequired: false,
+          auth0Logout: true,
+          issuerBaseURL: ISSUER_BASE_URL,
+          baseURL: process.env.ENVIRONMENT === 'DEV' ? DEV_BASE_URL : PROD_BASE_URL,
+          clientID: CLIENT_ID ,
+          secret: SECRET,
           idpLogout: true,
         })
       );
+      // this.app.use(cors);
       this.app.use(logger('dev'));
       this.app.use(express.urlencoded({ extended: true }))
       this.app.use(express.json());
       this.app.use(function (req: any, res: any, next: any) {
-        res.header('Access-Control-Allow-Origin', '*');
+        // res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
         next();
       });
-
     }
   }
 
@@ -56,12 +65,22 @@ class App {
   private static routes(): void {
     const { app, deviceManagerController } = this;
 
-    app.post('/device-manager', (req: any, res: any) => {
-      const deviceId: string = req.body.deviceId;
-      const farmAddress: string = req.body.farmAddress; //static ip or subdomain
+    app.get('/', (req, res) => {
+      // res.send('hello')
+      res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged Out');
+    })
 
-      console.log('Device ID: ' + deviceId)
-      console.log('LAN Root Address: ' + farmAddress)
+    app.get('/dashboard', requiresAuth(), (req, res) => {
+      res.send(JSON.stringify(req.oidc.user));
+    })
+
+    //need ID of device here
+    app.post('/device-manager/:id', requiresAuth(), (req: any, res: any) => {
+      const deviceId = req.query.id;
+      const farmAddress: string = req.body.farmAddress; //static ip or subdomain root.bridgelabs.biz
+
+      console.log('Device ID: ' + deviceId);
+      console.log('LAN Root Address: ' + farmAddress);
 
       //req will have device data needed to make call like remote server location, device id
       return deviceManagerController.powerOff(deviceId, farmAddress)
--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,46 @@
 import App from './App';
 import dotenv from 'dotenv';
 import https from 'https';
+import http from 'https';
 import fs from 'fs';
 
 //access to environment variables
 dotenv.config({ path: './config/.env' });
 
 const HTTPS_PORT = process.env.HTTPS_PORT;
-
-//read private key - should be an env variable or dynamically set (ec2-user as ex.)
-const privateKey = fs.readFileSync('/etc/pki/tls/private/privkey.key', 'utf8');
-
-//read private key 
-const certificate = fs.readFileSync('/etc/pki/tls/certs/custom.crt', 'utf8');
-
-//create credentials object, should build a type for this
-const credentials = { key: privateKey, cert: certificate };
-
-//build App with given port
+const HTTP_PORT = process.env.HTTP_PORT;
+const ENVIRONMENT = process.env.ENVIRONMENT;
 const app = App.buildApp();
 
-const httpsServer = https.createServer(credentials, app);
+let server = null;
+
+if (ENVIRONMENT === 'DEV') {
+    server = http.createServer(app);
+} else {
+    const privateKey = fs.readFileSync('/etc/pki/tls/private/privkey.key', 'utf8');
+    const certificate = fs.readFileSync('/etc/pki/tls/certs/custom.crt', 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
+
+    server = https.createServer(credentials, app);
+}
+
+app.get('/login', (req, res) => {
+    res.send('hello')
+    // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged Out');
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Example app listening at http://localhost:${HTTP_PORT}`
+        
+  )
+})
 
 //now listening for client requests 
-httpsServer.listen(HTTPS_PORT);
+// server.listen(process.env.ENVIRONMENT === 'DEV' ? HTTP_PORT : HTTPS_PORT);
 
-//output port in usage
-httpsServer.on('listening', () => {
-    const addr = httpsServer.address();
-    const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+// server.on('listening', () => {
+//     const addr = server.address();
+//     const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
 
-    console.log('Listening on ' + bind)
-});
+//     console.log('HTTP Listening on ' + bind)
+// });
