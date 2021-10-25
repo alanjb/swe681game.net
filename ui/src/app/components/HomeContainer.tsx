3e91e65c9132448832e5d919d6119925cf7cdc99
Alan <boyce.alan15@gmail.com>
2021-10-25 13:54:02 -0400

    adding joi validaiton package, updating home container

--- ui/src/app/components/HomeContainer.tsx
+++ ui/src/app/components/HomeContainer.tsx
@@ -1,8 +1,24 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
+import { Container, Row, Col } from "reactstrap";
 
 const HomeContainer = () => (
   <Fragment>
-    Welcome to SWE681 game home page
+    <div className="home-container">
+      <Container className="home-header themed-container" fluid={true}>
+        <Row>
+          <Col>
+            <h2 className="header-text text-center ">Welcome to Poker 101</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="sub-text text-center">
+              <p>Sign in to start playing</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   </Fragment>
 );
 
