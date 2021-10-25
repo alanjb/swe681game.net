81fb9dcece87daf7f216f4252e7450889747afde
Alan <boyce.alan15@gmail.com>
2021-10-25 06:22:33 -0400

    updates to dashboard functions

--- ui/src/app/components/DashboardContainer.tsx
+++ ui/src/app/components/DashboardContainer.tsx
@@ -54,29 +54,6 @@ class DeviceManagerContainer extends Component {
 
     history.push(`/game/${1}`);
   }
-
-  createGame() {
-    console.log('creating game...');
-
-    // const playersArray = ['john@gmail.com', 'jim@gmail.com'];
-  
-    //get other settings - use a create new game modal 
-  //   axios
-  //     .post(`http://localhost:8000/api/game/create`, {playersArray: playersArray})
-  //     .then(res => {
-  
-  //       if(res.data){
-  //         // const { history } = this.props;
-  //         const { gameId } = res.data;
-  //         //redirect to GameContainer using router and game id
-  //         alert("Starting game...redirecting... ");
-  //         history.push(`/game/${gameId}`);
-  //       }
-  //     })
-  //     .catch(error => {
-  //       alert("Error! Failed to create game: " + error);
-  //     })
-  }
 }
 
 type State = {
