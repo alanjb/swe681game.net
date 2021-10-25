d83a40d0fd5e5cfdcdfae6ead98662c408a845e5
Alan <boyce.alan15@gmail.com>
2021-10-25 07:43:07 -0400

    minor updates

--- api/src/game/models/Player.ts
+++ api/src/game/models/Player.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import { CardSchema } from "./Card";
 
 export const PlayerSchema = new mongoose.Schema({
   id: String,
   folded: Boolean,
   isDealer: Boolean,
   points: Number,
-  hand: [],
+  hand: [CardSchema],
 });
 
 export const Player = mongoose.model("Player", PlayerSchema);
