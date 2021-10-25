d576f53783f13e992580b71dd3c14c5f7c111ef5
Alan <boyce.alan15@gmail.com>
2021-10-25 06:00:58 -0400

    dashboard ui updates

--- ui/src/app/components/DashboardContainer.tsx
+++ ui/src/app/components/DashboardContainer.tsx
@@ -1,7 +1,7 @@
 import { Component, Fragment } from 'react';
 import history from '../routing/history';
 import CreateGameModal from '../../game/components/CreateGameModal';
-import { Button } from 'reactstrap';
+import { Button, Col, Container, Row } from 'reactstrap';
 import Game from '../../game/models/Game';
 
 class DeviceManagerContainer extends Component {
@@ -15,12 +15,30 @@ class DeviceManagerContainer extends Component {
 
     return (
       <Fragment>
-          <div className="dashboard-container">
-            <Button color="info" onClick={toggleCreateGameModal}>
-              Create new game
-            </Button>
-          </div>
-          <CreateGameModal isOpen={isCreateGameModalOpen} toggle={toggleCreateGameModal} created={gameCreated} />
+        <div className="dashboard-container">
+          <Container className="dashboard-content themed-container" fluid={true}>
+            <Row>
+              <Col>
+                <h2 className="dashboard-header">Dashboard</h2>
+              </Col>
+              <Col>
+                <div className="create-button-container justify-content-end">
+                  <Button className="align-self-end" color="info" onClick={toggleCreateGameModal}>
+                    Create new game
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div>
+                  test
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <CreateGameModal isOpen={isCreateGameModalOpen} toggle={toggleCreateGameModal} created={gameCreated} />
       </Fragment>
     );
   }
